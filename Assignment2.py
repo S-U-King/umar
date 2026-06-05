ba6952c0657681390eb549b51592da7409a9a713
S.Umar Farooq <eabumar54@gmail.com>
2026-06-05 11:31:48 +0500

    class 2 assignment
    all task clear

---
+++ Assignment2.py
@@ -0,0 +1,74 @@
+### Task no:1
+
+name = "Syed Umar Farooq"
+age = 17
+city = "Karachi"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Is Student:", is_student)
+
+### Task no:2
+
+name = "Ali"
+age = 18
+isStudent = True
+
+print("Data type of name:", type(name))
+print("Data type of age:", type(age))
+print("Data type of isStudent:", type(isStudent))
+
+### Task no:3
+
+marks = "95"
+
+converted_marks = int(marks)
+
+print("Converted value:", converted_marks)
+print("Data type:", type(converted_marks))
+
+### Task no:4
+
+phoneNumber = 123456789
+
+converted_phoneNumber = str(phoneNumber)
+
+print("Converted value:", converted_phoneNumber)
+print("Data type:", type(converted_phoneNumber))
+
+### Task no:5
+
+userName = "Ahmed"
+
+converted_userName = bool(userName)
+
+print("Converted value:", converted_userName)
+print("Data type:", type(converted_userName))
+
+### Task no:6
+
+Name = "Syed Umar Farooq"
+Age = 17
+Fav_color = "Blue"
+Is_student = True
+
+print("====================================")
+print("        PERSONAL INFORMATION        ")
+print("====================================")
+print(f"Name:               {Name}")
+print(f"Age:                {Age} years old")
+print(f"Favorite Color:     {Fav_color}")
+print(f"Student Status:     {Is_student}")
+print("====================================")
+
+### Bonus Task
+
+num = "100"
+
+converted_num = int(num)
+
+result = converted_num + 50
+
+print(f"{converted_num} + 50 = {result}")
